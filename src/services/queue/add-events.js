2761fe47805265213029ba7eaa380815bda5c3ca
Akeeb Ismail <ismailakeeb@gmail.com>
2021-02-22 17:24:47 +0100

    pub sub

--- src/services/queue/add-events.js
+++ src/services/queue/add-events.js
@@ -2,7 +2,6 @@ const attachEvents = (connection, {onError, onClose}) => {
     const add = (event, handler) => {
         if (typeof handler === 'function') connection.on(event, handler)
     }
-
     add('error', onError)
     add('close', onClose)
 }
